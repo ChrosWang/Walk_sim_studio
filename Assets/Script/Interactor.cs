e951e44f2961a0dca439248ec6973df4ef33d028
Chros <chroswang@gmail.com>
2021-10-24 05:15:41 +0800

    Add a new first person drifter with audio source

---
+++ Assets/Script/Interactor.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+   // public LayerMask interactableLayermask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    //  RaycastHit hit;
+        
+   //     if(Physics.Raycast(GetComponent<Camera>().main.transform.position, GetComponent<Camera>().main.transform.forward, out hit, 2, interactableLayermask))
+    //    {
+   //         Debug.Log(hit.collider.name);
+    //    }
+    }
+}
